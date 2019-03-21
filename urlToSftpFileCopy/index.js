11c359aa215f9fb06bf49de3e4054931bd8d24b1
Yavor Georgiev <yavor@fivequarters.io>
2019-03-21 13:56:25 -0700

    Copying file from URL to SFTP sample

---
+++ urlToSftpFileCopy/index.js
@@ -0,0 +1,52 @@
+const Fs = require('fs');
+const Path = require('path');
+const Axios = require('axios');
+const Client = require('ssh2-sftp-client');
+
+module.exports = async (ctx, cb) => {
+    const fileUrl = ctx.body.fileToCopy;
+    const fileName = fileUrl.substring(fileUrl.lastIndexOf('/')+1);
+    const tmpFilePath = Path.resolve('/tmp', fileName);
+    const remoteFilePath =  Path.resolve(ctx.configuration.SFTP_PATH, fileName);
+    
+    try {
+        await downloadImage(fileUrl, tmpFilePath);
+        let response = await uploadImage(ctx.configuration.SFTP_HOST,
+            ctx.configuration.SFTP_PORT,
+            ctx.configuration.SFTP_USER,
+            ctx.configuration.SFTP_PASSWORD,
+            tmpFilePath,
+            remoteFilePath);
+        cb(null, {body: response});
+    } catch (e) {
+        cb(e);
+    }
+}
+
+async function uploadImage(host, port, username, password, localFilePath, remoteFilePath){
+    let sftp = new Client();
+    await sftp.connect({
+        host: host,
+        port: port,
+        username: username,
+        password: password
+    });
+    let response = await sftp.fastPut(localFilePath, remoteFilePath);
+    sftp.end();
+    return response;
+}
+
+async function downloadImage(fileUrl, tmpFilePath){
+    const writer = Fs.createWriteStream(tmpFilePath);
+    
+    const response = await Axios({
+        url: fileUrl,
+        method: 'GET',
+        responseType: 'stream'
+    });
+
+    response.data.pipe(writer);
+
+    writer.on('finish', () => { return; });
+    writer.on('error', (e) => { throw e; });
+}
